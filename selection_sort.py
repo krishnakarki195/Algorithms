9d7013e7a91cca26bf4fea64bb6d71a54f50b2b4
Krishna Karki <krishna.karki195@gmail.com>
2019-08-27 11:58:01 -0700

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(input_list):
+	for i in range(len(input_list)):
+		minimum = i
+		for j in range(i+1,len(input_list)):
+			if input_list[j] < input_list[minimum]:
+				minimum = j
+		input_list[minimum], input_list[i] = input_list[i], input_list[minimum]
+	return input_list
