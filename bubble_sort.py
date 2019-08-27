31f1f873d0798eb9f09cda3d309a3380dafd1a63
Krishna Karki <krishna.karki195@gmail.com>
2019-08-27 12:05:01 -0700

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,6 @@
+def bubble_sort(input_list):
+	for i in range(0,len(input_list)):
+		for j in range(i,len(input_list)):
+			if input_list[i] > input_list[j]:
+				input_list[i], input_list[j] = input_list[j], input_list[i]
+	return input_list
