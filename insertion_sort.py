d7556e95c2825f0d0070d40ef590f394e01ac97e
Krishna Karki <krishna.karki195@gmail.com>
2019-08-27 12:03:38 -0700

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(input_list):
+	for i in range(0,len(input_list)):
+		cursor = input_list[i]
+		position = i
+		while position > 0 and input_list[position-1] > cursor:
+			input_list[position] = input_list[position-1]
+			position  = position - 1
+		input_list[position] = cursor
+	return input_list
