ae124b138feea49f00d03f5318be0612e3b89880
Krishna Karki <krishna.karki195@gmail.com>
2019-08-27 12:05:49 -0700

    Bubble Sort added

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,14 @@
+"""
+author: Krishna Karki
+Created date: 8/27/2019
+description: written in python
+"""
+
 def bubble_sort(input_list):
 	for i in range(0,len(input_list)):
 		for j in range(i,len(input_list)):
 			if input_list[i] > input_list[j]:
 				input_list[i], input_list[j] = input_list[j], input_list[i]
 	return input_list
+
+
