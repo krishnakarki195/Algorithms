1e7e3b43990879a07bafac489de3d68a4562287c
Krishna Karki <krishna.karki195@gmail.com>
2019-08-28 11:13:27 -0700

    Create merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,23 @@
+def merge(left, right, merged_list):
+	left_cursor,right_cursor = 0,0
+	while left_cursor < len(left) and right_cursor < len(right):
+		if left[left_cursor] <= right[right_cursor]:
+			merged_list[left_cursor+right_cursor] = left[left_cursor]
+			left_cursor += 1
+		else:
+			merged_list[left_cursor+right_cursor] = right[right_cursor]
+			right_cursor += 1
+	while left_cursor < len(left):
+		merged_list[left_cursor+right_cursor] = left[left_cursor]
+		left_cursor += 1
+	while right_cursor < len(right):
+		merged_list[left_cursor+right_cursor] = right[right_cursor]
+		right_cursor += 1
+	return merged_list
+
+def merge_sort(input_list):
+	if len(input_list) <= 1:
+		return input_list
+	mid = len(input_list) // 2
+	left,right = merge_sort(input_list[:mid]),merge_sort(input_list[mid:])
+	return merge(left,right,input_list[:])
