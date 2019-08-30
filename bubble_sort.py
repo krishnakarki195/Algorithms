1dba33677654b04e57a3d6e279b14c2bdb41f1fd
Krishna Karki <krishna.karki195@gmail.com>
2019-08-30 12:31:19 -0700

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,20 +4,17 @@ created date: 8/27/2019
 description: bubble sort written in python
 """
 
-class InsertionSort(object):
-	def insertion_sort(self, input_list):
+class BubbleSort(object):
+	def bubble_sort(self, input_list):
 		for i in range(0,len(input_list)):
-			cursor = input_list[i]
-			position = i
-			while position > 0 and input_list[position-1] > cursor:
-				input_list[position] = input_list[position-1]
-				position  = position - 1
-			input_list[position] = cursor
+			for j in range(i,len(input_list)):
+				if input_list[i] > input_list[j]:
+					input_list[i], input_list[j] = input_list[j], input_list[i]
 		return input_list
 
 
-my_obj = InsertionSort()
+my_obj = BubbleSort()
 my_list = [2,4,5,2,1,7,8,9,5,6]
 print("Befor sorting: ", my_list)
-my_list = my_obj.insertion_sort(my_list)
+my_list = my_obj.bubble_sort(my_list)
 print("After sorting: ", my_list)
