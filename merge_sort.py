3c028bca779f63f221d075719213e9cce2398063
Krishna Karki <krishna.karki195@gmail.com>
2019-08-30 12:22:47 -0700

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -4,26 +4,33 @@ created date: 8/28/2019
 description: written in python
 """
 
-def merge(left, right, merged_list):
-	left_cursor,right_cursor = 0,0
-	while left_cursor < len(left) and right_cursor < len(right):
-		if left[left_cursor] <= right[right_cursor]:
+class MergeSort(object):
+	def merge_sort(self, input_list):
+		if len(input_list) <= 1:
+			return input_list
+		mid = len(input_list) // 2
+		left, right = self.merge_sort(input_list[:mid]), self.merge_sort(input_list[mid:])
+		return self.merge(left,right,input_list[:])
+	
+	def merge(self, left, right, merged_list):
+		left_cursor,right_cursor = 0,0
+		while left_cursor < len(left) and right_cursor < len(right):
+			if left[left_cursor] <= right[right_cursor]:
+				merged_list[left_cursor+right_cursor] = left[left_cursor]
+				left_cursor += 1
+			else:
+				merged_list[left_cursor+right_cursor] = right[right_cursor]
+				right_cursor += 1
+		while left_cursor < len(left):
 			merged_list[left_cursor+right_cursor] = left[left_cursor]
 			left_cursor += 1
-		else:
+		while right_cursor < len(right):
 			merged_list[left_cursor+right_cursor] = right[right_cursor]
 			right_cursor += 1
-	while left_cursor < len(left):
-		merged_list[left_cursor+right_cursor] = left[left_cursor]
-		left_cursor += 1
-	while right_cursor < len(right):
-		merged_list[left_cursor+right_cursor] = right[right_cursor]
-		right_cursor += 1
-	return merged_list
+		return merged_list
 
-def merge_sort(input_list):
-	if len(input_list) <= 1:
-		return input_list
-	mid = len(input_list) // 2
-	left,right = merge_sort(input_list[:mid]),merge_sort(input_list[mid:])
-	return merge(left,right,input_list[:])
+my_obj = MergeSort()
+my_list = [2,4,5,2,1,7,8,9,5,6]
+print("Befor sorting: ", my_list)
+my_list = my_obj.merge_sort(my_list)
+print("After sorting: ", my_list)
